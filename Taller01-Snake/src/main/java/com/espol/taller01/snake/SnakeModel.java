cfce9c31ec6fffb74d9519b620e24a20cfd05875
lsferivera <154564202+lsferivera@users.noreply.github.com>
2024-10-09 15:28:20 -0500

    Cambio de txt en boton|Cambio de color cabeza
    +Se cambio el texto del boton StartGameButton en GUIView.java
    
    +Se cambio el color de la cabeza de la serpiente en SnakeModel.java

--- Taller01-Snake/src/main/java/com/espol/taller01/snake/SnakeModel.java
+++ Taller01-Snake/src/main/java/com/espol/taller01/snake/SnakeModel.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.event.KeyEvent;
 import java.util.LinkedList;
 
+import javax.swing.text.Position;
+
 
 /**
  * Simple classic game of snake. The snake is put in the middle of the game pane
@@ -55,7 +57,7 @@ public class SnakeModel extends GameModel {
 	private static final GameTile SNAKE_TILE = new RectangularTile(Color.darkGray);
 
 	/** Graphical representation of the snake's head */
-	private static final GameTile SNAKE_HEAD_TILE = new RectangularTile(Color.GRAY);
+	private static final GameTile SNAKE_HEAD_TILE = new RectangularTile(Color.LIGHT_GRAY);
 	
 	/** Graphical representation of a blank tile. */
 	private static final GameTile BLANK_TILE = new GameTile();
